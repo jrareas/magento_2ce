98bc1c34b998a9e6e11b9c278a6b71ea429fc305
Jose Areas <jrareas@gmail.com>
2021-01-25 21:54:43 -0500

    Addressing permission issues

---
+++ app/docker/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+/var/www/magento/bin/magento setup:install \
+--base-url=http://$MAGENTO_HOST \
+--db-host=$MAGENTO_DATABASE_HOST \
+--db-name=$MAGENTO_DATABASE_NAME \
+--db-user=$MAGENTO_DATABASE_USER \
+--db-password=$MAGENTO_DATABASE_PASSWORD \
+--admin-firstname=$MAGENTO_ADMIN_FIRST_NAME \
+--admin-lastname=$MAGENTO_ADMIN_LAST_NAME \
+--admin-email=$MAGENTO_ADMIN_EMAIL \
+--admin-user=$MAGENTO_ADMIN_USER \
+--admin-password=$MAGENTO_ADMIN_PASSWORD \
+--currency=$MAGENTO_CURRENCY \
+--timezone=$MAGENTO_TIMEZONE \
+--use-rewrites=1 \
+--elasticsearch-host=$ELASTICSEARCH_HOST \
+--elasticsearch-port=$ELASTICSEARCH_PORT_NUMBER
