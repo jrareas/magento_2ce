6a80db7f2da921c1fc67c93f0db4503ead6d9bc6
Jose Areas <jrareas@gmail.com>
2021-01-25 19:46:11 -0500

    Addressing permission issues

--- app/docker/start.sh
+++ app/docker/start.sh
@@ -29,5 +29,5 @@ su www-data
 php /var/www/magento/bin/magento module:enable --all
 php /var/www/magento/bin/magento setup:di:compile;
 
-exit # exit www-data user
-/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+#exit # exit www-data user
+su /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
